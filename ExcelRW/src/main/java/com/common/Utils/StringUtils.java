60925628b40cab7299855e07c3086c3567736a56
ak <18373709268@163.com>
2024-04-15 17:30:43 +0800

    add StringUtils

---
+++ ExcelRW/src/main/java/com/common/Utils/StringUtils.java
@@ -0,0 +1,20 @@
+package com.common.Utils;
+
+/**
+ * @ClassName StringUtils
+ * @Description: 字符工具类
+ * @Author: ak
+ * @CreateDate: 2024/4/15 17:24
+ * @UpdateUser:
+ * @UpdateDate: 2024/4/15 17:24
+ * @UpdateRemark:
+ * @Version: 1.0
+ */
+
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+    private static final String NULLSTR = "";
+
+
+
+
+}
